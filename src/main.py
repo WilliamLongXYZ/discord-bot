e4fd3f7ca25caa1db0bdc094766e0d1d508f108f
William Long <will@williamlong.xyz>
2021-08-11 19:39:19 -0500

    Created notice when online.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-print("Hello world!")
+# https://discord.com/oauth2/authorize?client_id=875157389626716170&scope=bot&permissions=8589934591
 
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    bot_channel = client.get_channel(875147778882412604)
+    await bot_channel.send("Online.")
+
+client.run('TOKEN')
