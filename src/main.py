3466acc5fa8f54c05f17542e19f1318e0182a1f3
William Long <will@williamlong.xyz>
2021-08-11 19:44:57 -0500

    Changed method to get token.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 # https://discord.com/oauth2/authorize?client_id=875157389626716170&scope=bot&permissions=8589934591
 
+import os
+
 import discord
+from dotenv import load_dotenv
+
 
 client = discord.Client()
 
@@ -9,4 +13,5 @@ async def on_ready():
     bot_channel = client.get_channel(875147778882412604)
     await bot_channel.send("Online.")
 
-client.run('TOKEN')
+load_dotenv()
+client.run(os.getenv('TOKEN'))
